5e9eb16b113db23bf2c9687c95e4a245c38af7d4
Adarsh Reddy <adarshreddy123@gmail.com>
2020-09-17 15:49:41 +0530

    Added Zxing library for scanning barcodes

--- app/src/main/java/com/demo/mishipay/MainActivity.kt
+++ app/src/main/java/com/demo/mishipay/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.demo.mishipay
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.integration.android.IntentResult
+import com.journeyapps.barcodescanner.CaptureActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : CaptureActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.scan_btn)
+
+        scan_btn.setOnClickListener {
+            //initializeScan()
+            IntentIntegrator(this).setCaptureActivity(MainActivity::class.java).initiateScan()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result: IntentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Scanned: " + result.formatName + "\n" + result.getContents(),
+                    Toast.LENGTH_LONG).show()
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
